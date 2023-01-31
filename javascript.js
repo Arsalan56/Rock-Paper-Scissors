a360d92328d562971f493500afe31c1ecae5549f
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-01-31 17:29:46 -0500

    Create a function to determine winner
    A function is created that determines the winner by taking in 2
    parameters, both the user and the computer's selection. A switch case is
    used to avoid if-statement flooding, and make the code more readable by
    breaking it down into separate sections.

--- javascript.js
+++ javascript.js
@@ -7,3 +7,34 @@ function getComputerChoice () {
     let rndm = Math.floor(Math.random()*3)
     return dict[rndm]
 }
+
+function play (playerSelection, computerSelection) {
+    switch (playerSelection.toLowerCase()) {
+        case 'rock':
+            if (computerSelection === 'Scissors') {
+                return 'You Win! Rock beats Scissors';
+            } else if (computerSelection === 'Paper'){
+                return 'You Lose! Paper beats Rock'
+            } else {
+                return 'You both played Rock, Tie!'
+            }
+
+        case 'paper':
+            if (computerSelection === 'Rock') {
+                return 'You Win! Paper beats Rock';
+            } else if (computerSelection === 'Scissors'){
+                return 'You Lose! Scissors beats Paper'
+            } else {
+                return 'You both played Paper, Tie!'
+            }
+
+        case 'scissors':
+            if (computerSelection === 'Paper') {
+                return 'You Win! Scissors beats Paper';
+            } else if (computerSelection === 'Rock') {
+                return 'You Lose! Rock beats Scissors'
+            } else {
+                return 'You both played Scissors, Tie!'
+            }
+    }
+}
