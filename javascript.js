de55631567a25269518d3dc2177235144b1b2f32
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-02-15 19:50:52 -0500

    Make game end when either side gets 5 points
    Create two functions which determine when to end the game and show
    results. Once the game ends and results are shown, both sides restart
    at 0 points.
    f

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice () {
     const dict = {
         0: 'Rock',
@@ -8,33 +11,55 @@ function getComputerChoice () {
     return dict[rndm]
 }
 
+function computerAdd() {
+    if (++computerScore >= 5) {
+        playerScore = 0;
+        computerScore = 0;
+        return true;
+    }
+};
+
+function playerAdd() {
+    if (++playerScore >= 5) {
+        playerScore = 0;
+        computerScore = 0;
+        return true;
+    }
+};
+
 function play (playerSelection, computerSelection) {
     switch (playerSelection.toLowerCase()) {
         case 'rock':
             if (computerSelection === 'Scissors') {
-                return 'You Win! Rock beats Scissors';
+                if (playerAdd() === true) return 'Game over, you Win!'; 
+                return `You Win! Rock beats Scissors ${playerScore} - ${computerScore}`;
             } else if (computerSelection === 'Paper'){
-                return 'You Lose! Paper beats Rock'
+                if (computerAdd() === true) return 'Game over, you Lose!';
+                return `You Lose! Paper beats Rock ${playerScore} - ${computerScore}`;
             } else {
-                return 'You both played Rock, Tie!'
+                return `You both played Rock, Tie! ${playerScore} - ${computerScore}`;
             }
 
         case 'paper':
             if (computerSelection === 'Rock') {
-                return 'You Win! Paper beats Rock';
+                if (playerAdd() === true) return 'Game over, you Win!'; 
+                return `You Win! Paper beats Rock ${playerScore} - ${computerScore}`;
             } else if (computerSelection === 'Scissors'){
-                return 'You Lose! Scissors beats Paper'
+                if (computerAdd() === true) return 'Game over, you Lose!';
+                return `You Lose! Scissors beats Paper ${playerScore} - ${computerScore}`;
             } else {
-                return 'You both played Paper, Tie!'
+                return `You both played Paper, Tie! ${playerScore} - ${computerScore}`;
             }
 
         case 'scissors':
             if (computerSelection === 'Paper') {
-                return 'You Win! Scissors beats Paper';
+                if (playerAdd() === true) return 'Game over, you Win!'; 
+                return `You Win! Scissors beats Paper ${playerScore} - ${computerScore}`;
             } else if (computerSelection === 'Rock') {
-                return 'You Lose! Rock beats Scissors'
+                if (computerAdd() === true) return 'Game over, you Lose!';
+                return `You Lose! Rock beats Scissors ${playerScore} - ${computerScore}`;
             } else {
-                return 'You both played Scissors, Tie!'
+                return `You both played Scissors, Tie! ${playerScore} - ${computerScore}`;
             }
     }
 }
@@ -42,11 +67,10 @@ function play (playerSelection, computerSelection) {
 function playRound (playerSelection) {
     const div = document.querySelector('div');
     const output = document.querySelector('p');
-
-    // const output = document.createElement('p');
     output.textContent = (play(playerSelection, getComputerChoice()));
     div.appendChild(output);
 }
 
 let buttons = document.querySelectorAll('button');
 buttons.forEach(btn => btn.addEventListener('click', () => playRound(btn.className)));
+
